6a2960c39faa0d9a821609f49c0775c35c7396d8
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-07-13 12:00:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get the input element by its id
+const inputField = document.getElementById('fname');
+
+// Add an event listener for the 'blur' event (when the input loses focus)
+inputField.addEventListener('blur', function() {
+  // Convert the value of the input field to uppercase
+  this.value = this.value.toUpperCase();
+});
